8b4babf2c2de9a2fefdf68fe240b0bb563241040
isLinXu <2267379130@qq.com>
2023-08-06 21:21:08 +0800

    [Saas][utils] add yaml_tool.py

---
+++ Saas/utils/yaml_tool.py
@@ -0,0 +1,23 @@
+import yaml
+
+
+def get_classes_from_yaml(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # 确保YAML文件中的确有"classes"键
+    if 'classes' in data:
+        return data['classes']
+    else:
+        return None
+
+    # 使用函数并打印结果
+
+
+if __name__ == '__main__':
+    yaml_path = "../../data/config/lvis_cls_1203.yaml"
+    classes = get_classes_from_yaml(yaml_path)  # 将'your_file.yaml'替换为你的YAML文件路径
+    if classes is not None:
+        print(classes)
+    else:
+        print("Classes not found in the YAML file.")
